054243bf95aebeaaca4454e3600b008921dd94df
Roberta Barberena <barberenar@gmail.com>
2022-04-29 16:30:56 -0600

    Added GitHub link in footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return (<div className="App"><h1>Weather App</h1></div>);
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Roberta Barberena and is{" "}
+        <a
+          href="https://github.com/barberenar/weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
