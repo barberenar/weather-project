0e3105a8ae2a7327d1572e75f0c69610847115d3
Roberta Barberena <barberenar@gmail.com>
2022-04-29 18:03:00 -0600

    Made changes in styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
+import Weather from "./Weather";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Roberta Barberena and is{" "}
-        <a
-          href="https://github.com/barberenar/weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Roberta Barberena and is{" "}
+          <a
+            href="https://github.com/barberenar/weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
